79b0c29d29767c5274ec9dbb2206d95e5f25753e
Legrain Samuel <samuel.legrain@bstorm.be>
2022-03-31 09:42:13 +0200

    Ajoutez des fichiers projet.

---
+++ Exercice01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercice01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour, veuillez renseigner votre nom :");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Merci, et maintenant, renseignez votre prénom :");
+            string prenom = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Bienvenue {nom} {prenom}!");
+        }
+    }
+}
