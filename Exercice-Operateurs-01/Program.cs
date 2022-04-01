ef99b357c7253cd799f8c376f07ceab3b8257879
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-01 16:02:16 +0200

    Add Exercices Opérateurs

---
+++ Exercice-Operateurs-01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercice_Operateurs_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Version if imbriqués
+            //int nb1, nb2;
+            //Console.WriteLine("Bonjour, veuillez introduire un premier nombre :");
+            //string input = Console.ReadLine();
+            //if (!int.TryParse(input, out nb1)) Console.WriteLine($"'{input}' n'est pas un nombre...");
+            //else
+            //{
+            //    Console.WriteLine("Ensuite, veuillez introduire un second nombre :");
+            //    input = Console.ReadLine();
+            //    if(!int.TryParse(input, out nb2)) Console.WriteLine($"'{input}' n'est pas un nombre...");
+            //    else Console.WriteLine($"Nombre 1 : {nb1} - Nombre 2 : {nb2}\nDivision entière : {nb1 / nb2} | Modulo : {nb1 % nb2} | Division : {(float)nb1/nb2}");
+            //} 
+            #endregion
+
+            #region Version if séparé
+            int nb1 = 0, nb2 = 0;
+            bool isOk = true;
+            Console.WriteLine("Bonjour, veuillez introduire un premier nombre :");
+            string input = Console.ReadLine();
+            isOk = int.TryParse(input, out nb1);
+            if (isOk)
+            {
+                Console.WriteLine("Ensuite, veuillez introduire un second nombre :");
+                input = Console.ReadLine();
+                isOk = int.TryParse(input, out nb2);
+            }
+
+            if (!isOk) Console.WriteLine($"'{input}' n'est pas un nombre...");
+            else Console.WriteLine($"Nombre 1 : {nb1} - Nombre 2 : {nb2}\nDivision entière : {nb1 / nb2} | Modulo : {nb1 % nb2} | Division : {(float)nb1 / nb2}"); 
+            #endregion
+        }
+    }
+}
---
+++ Exercice-Operateurs-02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercice_Operateurs_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenu, veuillez introduire un code BBAN à 12 chiffres :");
+            string bban = Console.ReadLine();               //0123 4567 8912
+            if (bban.Length == 12)
+            {
+                string bban_ten_first = bban.Substring(0, 10);   //0123 4567 89
+                string bban_two_last = bban.Substring(10, 2);    //12
+                long ten_number;
+                if (long.TryParse(bban_ten_first, out ten_number)) {
+                    short two_number;
+                    if (short.TryParse(bban_two_last, out two_number))
+                    {
+                        short check_value = (short)(ten_number % 97);
+                        if (check_value == two_number || (check_value == 0 && two_number == 97)) Console.WriteLine("OK");
+                        else Console.WriteLine("KO");
+                    }
+                    else Console.WriteLine("Code invalide");
+                }
+                else {
+                    Console.WriteLine("Code invalide");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Il n'y a pas 12 chiffres dans votre code...");
+            }
+
+        }
+    }
+}
