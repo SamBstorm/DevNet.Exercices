2ffdcdd829e182251f1499e2236d9350a2d8eb6c
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-04 15:00:14 +0200

    Add Exercices Boucles 1 & 2

---
+++ Demo-Boucles-01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercice_Boucles_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //F0 : 0 , F1 : 1, F2 : F0+F1 1, F3 : F1+F2 2, ...., F25 : 75025
+            int prev = 0, next = 1;
+            Console.WriteLine("F0 : " + prev);
+            Console.WriteLine("F1 : " + next);
+            for (int i = 2; i <= 25; i++)
+            {
+                int result = prev + next;
+                prev = next;
+                next = result;
+                Console.WriteLine($"F{i} : {next}");
+            }
+        }
+    }
+}
---
+++ Exercice-Boucles-02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercice_Boucles_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbFact;
+            Console.WriteLine("Veuillez indiquer un nombre :");
+            while (!int.TryParse(Console.ReadLine(), out nbFact)) {
+                Console.WriteLine("Valeur non valide, veuillez entrer un nouveau nombre :");
+            }
+
+            long result = 1;
+            for (int i = nbFact; i > 1 ; i--)
+            {
+                result *= i;
+            }
+            Console.WriteLine($"Le nombre factoriel de {nbFact} est {result}");
+
+
+            //F6 = 6 * 5 * 4 * 3 * 2 
+        }
+    }
+}
