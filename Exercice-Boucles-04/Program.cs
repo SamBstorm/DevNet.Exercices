e4c8353e06f06eb29b87b4acd17ac2ef19752588
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-04 17:03:49 +0200

    Add Exercice Boucles 04

---
+++ Exercice-Boucles-04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercice_Boucles_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int table = 1; table <= 5; table++)
+            {
+                for (int multiple = 1; multiple <= 20; multiple++)
+                {
+                    Console.Write($"{table}x{multiple}={multiple * table} ; ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
