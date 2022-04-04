7f5cd6e5bf611cf8c408ba264a5c82cbc9fd6dad
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-04 16:35:42 +0200

    Add Exercice Boucles 3

---
+++ Exercice-Boucles-03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercice_Boucles_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit;
+            Console.WriteLine("Indiquez le nombre de nombres premiers voulu (au minimum 1):");
+            while (!int.TryParse(Console.ReadLine(), out limit) || limit < 1 )
+            {
+                Console.WriteLine("Erreur veuillez entrer un autre nombre");
+            }
+
+            int cpt = 2;
+            int cptIsPrime = 0;
+            do
+            {
+                bool isPrime = true;
+                int buffer = cpt/2;
+                while (isPrime && buffer > 1)
+                {
+                    if (cpt % buffer == 0) isPrime = false;
+                    buffer--;
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine(cpt);
+                    cptIsPrime++;
+                }
+                cpt++;
+            } while (cptIsPrime < limit);
+        }
+    }
+}
