32a9d56afd9656f174ebe4c757c79a8760dc49b8
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-05 16:20:21 +0200

    Update Correction Boucles 3

--- Exercice-Boucles-03/Program.cs
+++ Exercice-Boucles-03/Program.cs
@@ -6,31 +6,56 @@ namespace Exercice_Boucles_03
     {
         static void Main(string[] args)
         {
+            #region Avec While
+            //int limit;
+            //Console.WriteLine("Indiquez le nombre de nombres premiers voulu (au minimum 1):");
+            //while (!int.TryParse(Console.ReadLine(), out limit) || limit < 1)
+            //{
+            //    Console.WriteLine("Erreur veuillez entrer un autre nombre");
+            //}
+
+            //int cpt = 2;
+            //int cptIsPrime = 0;
+            //do
+            //{
+            //    bool isPrime = true;
+            //    int buffer = cpt / 2;
+            //    while (isPrime && buffer > 1)
+            //    {
+            //        if (cpt % buffer == 0) isPrime = false;
+            //        buffer--;
+            //    }
+            //    if (isPrime)
+            //    {
+            //        Console.WriteLine(cpt);
+            //        cptIsPrime++;
+            //    }
+            //    cpt++;
+            //} while (cptIsPrime < limit); 
+            #endregion
+            #region Avec For
             int limit;
             Console.WriteLine("Indiquez le nombre de nombres premiers voulu (au minimum 1):");
-            while (!int.TryParse(Console.ReadLine(), out limit) || limit < 1 )
+            while (!int.TryParse(Console.ReadLine(), out limit) || limit < 1)
             {
                 Console.WriteLine("Erreur veuillez entrer un autre nombre");
             }
 
             int cpt = 2;
-            int cptIsPrime = 0;
-            do
+            for (int cptIsPrime = 0; cptIsPrime < limit; cpt++)
             {
                 bool isPrime = true;
-                int buffer = cpt/2;
-                while (isPrime && buffer > 1)
+                for (int buffer = cpt / 2; isPrime && buffer > 1; buffer--)
                 {
                     if (cpt % buffer == 0) isPrime = false;
-                    buffer--;
                 }
                 if (isPrime)
                 {
                     Console.WriteLine(cpt);
                     cptIsPrime++;
                 }
-                cpt++;
-            } while (cptIsPrime < limit);
+            }
+            #endregion
         }
     }
 }
