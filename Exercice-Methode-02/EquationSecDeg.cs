3ce3204eb58fd285f5c694a9df84eb887d8bc33f
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-13 10:44:57 +0200

    Add Correction Methode 02

---
+++ Exercice-Methode-02/EquationSecDeg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Methode_02
+{
+    public struct EquationSecDeg
+    {
+        public double A, B, C;
+
+        //public bool Resoudre(out double? X1, out double? X2) {
+        //    double delta = (B * B) - (4 * A * C);
+        //    X1 = X2 = null;
+        //    if (delta < 0 ) return false;
+        //    if (delta == 0) {
+        //        X2 = X1 = CalculX();
+        //    }
+        //    if (delta > 0) {
+        //        double root = Math.Sqrt(delta);
+        //        X1 = CalculX(-root);
+        //        X2 = CalculX(root);
+        //    }
+        //    return true;
+        //}
+
+        public bool Resoudre(out double? X1, out double? X2)
+        {
+            double delta = (B * B) - (4 * A * C);
+            X1 = X2 = null;
+            if (delta < 0) return false;
+            double root =(delta > 0)?Math.Sqrt(delta): 0;
+            X1 = CalculX(-root);
+            X2 = CalculX(root);
+            return true;
+        }
+
+        public double CalculX(double sqrtDelta = 0) {
+            return (-B + sqrtDelta) / (2 * A);
+        }
+    }
+}
---
+++ Exercice-Methode-02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercice_Methode_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EquationSecDeg eq;
+            Console.WriteLine("Sachant que l'équation sera Ax² + Bx + C ,");
+            Console.WriteLine("Veuillez introduire la valeur de A assigné à X² :");
+            while (!double.TryParse(Console.ReadLine(), out eq.A)) Console.WriteLine("Veuillez ré-essayer");
+            Console.WriteLine("Veuillez introduire la valeur de B assigné à X :");
+            while (!double.TryParse(Console.ReadLine(), out eq.B)) Console.WriteLine("Veuillez ré-essayer");
+            Console.WriteLine("Veuillez introduire la valeur de C assigné à l'unité supplémentaire :");
+            while (!double.TryParse(Console.ReadLine(), out eq.C)) Console.WriteLine("Veuillez ré-essayer");
+
+            if (eq.Resoudre(out double? x1, out double? x2)) {
+                if (x1 == x2) Console.WriteLine($"L'équation {eq.A}x² + {eq.B}x + {eq.C} donne pour valeur à X : {x1}.");
+                else Console.WriteLine($"L'équation {eq.A}x² + {eq.B}x + {eq.C} donne pour valeurs à X : {x1} ou {x2}.");
+            }
+            else Console.WriteLine($"L'équation {eq.A}x² + {eq.B}x + {eq.C} est impossible à résoudre...");
+        }
+    }
+}
