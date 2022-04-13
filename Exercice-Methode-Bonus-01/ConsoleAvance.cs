720741006c803d7a24c53e3a0ebabe916e222369
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-13 09:35:10 +0200

    Add correction Methode Bonus 01

---
+++ Exercice-Methode-Bonus-01/ConsoleAvance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Methode_Bonus_01
+{
+    public struct ConsoleAvance
+    {
+        public void WriteTab(List<string> tableau) {
+            foreach (string item in tableau)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void WriteTab(string[] tableau)
+        {
+            foreach (string item in tableau)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Exercice-Methode-Bonus-01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercice_Methode_Bonus_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tab = { "toto", "titi", "tutu" };
+            ConsoleAvance ca;
+            ca.WriteTab(tab);
+        }
+    }
+}
